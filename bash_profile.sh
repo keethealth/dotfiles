8e8a4e345743a5554c9d6a691ea1f09f1d6fd739
Phillip Novess <phillip@novess.com>
2019-05-20 17:15:49 -0500

    Update bash_profile.sh

--- bash_profile.sh
+++ bash_profile.sh
@@ -50,7 +50,6 @@ export PATH="$PATH:$HOME/.rvm/bin"
 [[ -s "$HOME/.profile" ]] && source "$HOME/.profile" # Load the default .profile
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
-
 #############################################
 # NVM - for managing versions of node
 #############################################
